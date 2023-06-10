0f6368455ed09d2f0051bf21423e52c072498122
Jonathan Lamptey <jonathan.lamptey@persol.net>
2023-06-10 09:08:00 +0000

    Validations check

--- HubtelAPI/Hubtel_payment_API/Controllers/WalletController.cs
+++ HubtelAPI/Hubtel_payment_API/Controllers/WalletController.cs
@@ -86,6 +86,9 @@ namespace Hubtel_payment_API.Controllers
             {
                 return StatusCode(StatusCodes.Status409Conflict);
             }
+            
+
+            
 
             //To prevent a single User to have more than 5 wallets
             var vUserCount = await _walletRepository.GetWalletCountAsync(details.WalletAccountNumber);
@@ -96,11 +99,14 @@ namespace Hubtel_payment_API.Controllers
             }
 
 
+
             //To prevent the user to save any type of wallet other than Card and Momo
-            if (!(details.Type.ToLower().Equals("Card") || details.Type.ToLower().Equals("momo")))
+            if (!(details.Type.ToLower().Equals("card") || details.Type.ToLower().Equals("momo")))
             {
                 throw new Exception($"Wallet type {details.Type} does not exist");
             }
+            
+
 
             //To prevent the user to save any account scheme other than Visa, MasterCard, MTN, AirtelTigo or Vodafone
             if (!(details.AccountScheme.ToUpper().Equals("VISA") || details.AccountScheme.ToUpper().Equals("MASTERCARD")
@@ -111,6 +117,58 @@ namespace Hubtel_payment_API.Controllers
             }
 
             //To only get first 6 digits of a Master Card or Visa Card number
+            
+
+            if(details.AccountNumber != null && details.Type.ToLower().Equals("momo") && details.WalletAccountNumber.Length > 9)
+            {
+                throw new Exception($"Momo Account Number {details.WalletAccountNumber} can not be more than 10 characters");
+
+
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("visa") && details.WalletAccountNumber.Length > 15)
+            {
+                throw new Exception($"Card  Number {details.WalletAccountNumber} can not be more than 16 characters");
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("visa") && details.WalletAccountNumber.Length > 15)
+            {
+                throw new Exception($"Card  Number {details.WalletAccountNumber} can not be more than 16 characters");
+
+            }
+
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("momo") && details.AccountScheme.ToLower().Equals("mtn") && !details.AccountNumber.StartsWith("055") || !details.AccountNumber.StartsWith("024") || !details.AccountNumber.StartsWith("059"))
+            {
+                
+                throw new Exception($"Account number is not a valid MTN Number");
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("momo") && details.AccountScheme.ToLower().Equals("airteltigo") && !details.AccountNumber.StartsWith("027") || !details.AccountNumber.StartsWith("056") || !details.AccountNumber.StartsWith("026") || !details.AccountNumber.StartsWith("057") )
+            {
+
+                throw new Exception($"Account number is not a valid AirtelTigo Number");
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("momo") && details.AccountScheme.ToLower().Equals("vodafone") && !details.AccountNumber.StartsWith("020") || !details.AccountNumber.StartsWith("050"))
+            {
+
+                throw new Exception($"Account number is not a valid Vodafone Number");
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("card") && details.AccountScheme.ToLower().Equals("visa") && !details.AccountNumber.StartsWith("4"))
+            {
+
+                throw new Exception($"Account number is not a valid Visa Card Number");
+
+            }
+            if (details.AccountNumber != null && details.Type.ToLower().Equals("card") && details.AccountScheme.ToLower().Equals("mastercard") && !details.AccountNumber.StartsWith("5"))
+            {
+
+                throw new Exception($"Account number is not a valid MasterCard Number");
+
+            }
+
+
             string strValue = string.Empty;
             if (details.AccountScheme.ToUpper().Equals("VISA") || details.AccountScheme.ToUpper().Equals("MASTERCARD"))
             {
